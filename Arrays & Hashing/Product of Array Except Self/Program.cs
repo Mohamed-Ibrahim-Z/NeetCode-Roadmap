136b73e9f712af33c00fa04f6cd319314f7a4af5
Mohamed Ibrahim <mohmed42010@gmail.com>
2023-01-07 20:08:45 +0200

    [Solved] TProduct of Array Except Self C#

---
+++ Arrays & Hashing/Product of Array Except Self/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] ProductExceptSelf(int[] nums)
+{
+    int[] result = new int[nums.Length];
+    int product = 1;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = product;
+        product *= nums[i];
+    }
+    product = 1;
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        result[i] *= product;
+        product *= nums[i];
+    }
+    return result;
+}
