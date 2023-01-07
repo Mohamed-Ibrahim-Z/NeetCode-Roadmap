8ae58e63124d92b5c25f968ac377c478caff698c
Mohamed Ibrahim <mohmed42010@gmail.com>
2023-01-07 19:57:30 +0200

    [Solved] Top K Frequent Elements C#

---
+++ Arrays & Hashing/Top K Frequent Elements/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] TopKFrequent(int[] nums, int k)
+{
+    Dictionary<int, int> dic = new Dictionary<int, int>();
+
+    foreach(int x in nums)
+    {
+        if (dic.ContainsKey(x))
+            dic[x]++;
+        else
+            dic.Add(x, 1);
+    }
+    var sortedDict = from entry in dic orderby entry.Value descending select entry;
+    int[] res = new int[k];
+    for(int i =0; i<k;i++)
+    {
+        res[i] = sortedDict.ElementAt(i).Key;
+    }
+    return res;
+}
