8c4b0f4a28ca30d68e0ba8de1402b477c21350eb
Mohamed Ibrahim <mohmed42010@gmail.com>
2023-01-07 13:31:54 +0200

    [Solved] Group Anagrams C#

---
+++ Arrays & Hashing/Group Anagrams/program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class program
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var dict = new Dictionary<string, IList<string>>();
+            foreach (var str in strs)
+            {
+                var key = GetKey(str);
+                if (dict.ContainsKey(key))
+                {
+                    dict[key].Add(str);
+                }
+                else
+                {
+                    dict.Add(key, new List<string> { str });
+                }
+            }
+
+            return new List<IList<string>>(dict.Values);
+
+        }
+
+        private string GetKey(string str)
+        {
+            var array = str.ToCharArray();
+            Array.Sort(array);
+            return new string(array);
+        }
+    }
+}
